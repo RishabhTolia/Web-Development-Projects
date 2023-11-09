3f346475fb5479490aa5384a670841105eff2b40
RishabhTolia <89206354+RishabhTolia@users.noreply.github.com>
2023-11-09 13:42:08 +0000

    Uploading from 'FullStackWebDevelopmentUdemy'
    Includes Beginner DIY Web Development projects. First Commit.

---
+++ intro-to-node/index.js
@@ -0,0 +1,15 @@
+
+// Generating SuperHero Names.
+// var superheroes = require("superheroes");
+//
+// var mySuperheroName = superheroes.random();
+//
+// console.log(mySuperheroName);
+
+
+// Generating SuperVillain Names.
+var supervillains = require("supervillains");
+
+var mySupervillainName = supervillains.random();
+
+console.log(mySupervillainName);
